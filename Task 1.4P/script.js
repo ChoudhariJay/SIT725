981c1762e12b7141a52492ec284c33a3320a1056
ChoudhariJay <choudharijay3@gmail.com>
2025-03-16 18:10:19 +1100

    Added Task 1.4P

---
+++ Task 1.4P/script.js
@@ -0,0 +1,4 @@
+// Select the button by its ID
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Hello! This is a JavaScript alert from For SIT725 Week1.");
+});
